af9296a423804b7316b6828dc1d8c0bb50bd7b61
Varunastra <dyadyavaleras@yandex.by>
2019-07-14 22:57:46 +0300

    Updates

---
+++ precache-manifest.730faef50304f792de39f28427229630.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440d452909b22e13349367864c93358b",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "004cc3ba949a891c2c8d",
+    "url": "/WeatherApp/static/css/main.84a64356.chunk.css"
+  },
+  {
+    "revision": "5857a963b09455ac7b2d",
+    "url": "/WeatherApp/static/js/2.88695804.chunk.js"
+  },
+  {
+    "revision": "004cc3ba949a891c2c8d",
+    "url": "/WeatherApp/static/js/main.4d8e8187.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  }
+]);
